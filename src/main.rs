b848f83b524ef04bc71ff9bd92e94d7f904a222a
Timon Vonk <timonv@gmail.com>
2024-07-13 15:53:34 +0200

    2. Getting user input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    language: String,
+
+    #[arg(short, long, default_value = "./")]
+    path: PathBuf,
+
+    query: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello Swiftide");
+    let args = Args::parse();
+
+    println!("{}", args.language);
+    println!("{}", args.path.to_string_lossy());
+    println!("{}", args.query);
 
     Ok(())
 }
