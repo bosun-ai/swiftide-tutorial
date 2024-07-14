79ad89a95cf005142198201e8d90ce6ca34036ec
Timon Vonk <timonv@gmail.com>
2024-07-14 19:08:44 +0200

    9. Getting swifty

--- src/main.rs
+++ src/main.rs
@@ -37,52 +37,45 @@ async fn main() -> Result<()> {
         .collection_name("swiftide-tutorial")
         .build()?;
 
-    index_markdown(&args.path, &openai, &qdrant).await?;
-    index_code(&args.language, &args.path, &openai, &qdrant).await?;
+    index_all(&args.language, &args.path, &openai, &qdrant).await?;
 
     Ok(())
 }
 
-async fn index_markdown(path: &PathBuf, openai: &OpenAI, qdrant: &Qdrant) -> Result<()> {
-    tracing::info!(path=?path, "Indexing markdown");
-
-    // Loads all markdown files into the pipeline
-    Pipeline::from_loader(FileLoader::new(path).with_extensions(&["md"]))
-        // The range ensures that chunks smaller than 50 characters are dropped, with a maximum size up to 1024 characters
-        .then_chunk(ChunkMarkdown::from_chunk_range(50..1024))
-        // Generate questions and answers and them to the metadata of the node
-        .then(MetadataQAText::new(openai.clone()))
-        // Embed chunks in batches of 100. By default the metadata in the node is included in the
-        // embedding
-        .then_in_batch(100, Embed::new(openai.clone()))
-        // Finally store the embeddings into Qdrant
-        .then_store_with(qdrant.clone())
-        .run()
-        .await
-}
-
-async fn index_code(
-    language: &str,
-    path: &PathBuf,
-    openai: &OpenAI,
-    qdrant: &Qdrant,
-) -> Result<()> {
+async fn index_all(language: &str, path: &PathBuf, openai: &OpenAI, qdrant: &Qdrant) -> Result<()> {
     tracing::info!(path=?path, language, "Indexing code");
 
-    // Parse the language to a supported one by the tree-sitter integration. Clap could also do
-    // this. It's also possible to directly pass the language as string to the transformer, but we
-    // save some time by getting the extensions.
     let language = SupportedLanguages::from_str(language)?;
+    let mut extensions = language.file_extensions().to_owned();
+    extensions.push("md");
 
-    Pipeline::from_loader(FileLoader::new(path).with_extensions(language.file_extensions()))
+    let (mut markdown, mut code) =
+        Pipeline::from_loader(FileLoader::new(path).with_extensions(&extensions))
+            .with_concurrency(50)
+            .split_by(|node| {
+                // Any errors at this point we just pass to 'markdown'
+                let Ok(node) = node else { return true };
+
+                // On true we go 'markdown', on false we go 'code'.
+                node.path.extension().map_or(true, |ext| ext == "md")
+            });
+
+    code = code
         // Uses tree-sitter to extract best effort blocks of code. We still keep the minimum
         // fairly high and double the chunk size
         .then_chunk(ChunkCode::try_for_language_and_chunk_size(
             language,
             50..1024,
         )?)
-        .then(MetadataQACode::new(openai.clone()))
-        .then_in_batch(100, Embed::new(openai.clone()))
+        .then(MetadataQACode::new(openai.clone()));
+
+    markdown = markdown
+        .then_chunk(ChunkMarkdown::from_chunk_range(50..1024))
+        // Generate questions and answers and them to the metadata of the node
+        .then(MetadataQAText::new(openai.clone()));
+
+    code.merge(markdown)
+        .then_in_batch(50, Embed::new(openai.clone()))
         .then_store_with(qdrant.clone())
         .run()
         .await
