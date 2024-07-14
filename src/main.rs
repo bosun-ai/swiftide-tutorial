e604dd60765d838890cdc09f5fb8f70add9da01e
Timon Vonk <timonv@gmail.com>
2024-07-14 10:12:54 +0200

    6. Enriching the data with questions and answers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
-use swiftide::{indexing::Pipeline, loaders::FileLoader, transformers::ChunkMarkdown};
+use swiftide::{
+    indexing::Pipeline,
+    integrations::openai::OpenAI,
+    loaders::FileLoader,
+    transformers::{ChunkMarkdown, MetadataQAText},
+};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,17 +27,25 @@ async fn main() -> Result<()> {
 
     let args = Args::parse();
 
-    index_markdown(&args.path).await?;
+    let openai = OpenAI::builder()
+        .default_embed_model("text-embedding-3-small")
+        .default_prompt_model("gpt-3.5")
+        .build()?;
+
+    index_markdown(&args.path, &openai).await?;
 
     Ok(())
 }
 
-async fn index_markdown(path: &PathBuf) -> Result<()> {
+async fn index_markdown(path: &PathBuf, openai: &OpenAI) -> Result<()> {
     tracing::info!(path=?path, "Indexing markdown");
 
     // Loads all markdown files into the pipeline
     Pipeline::from_loader(FileLoader::new(path).with_extensions(&[".md"]))
+        // The range ensures that chunks smaller than 50 characters are dropped, with a maximum size up to 1024 characters
         .then_chunk(ChunkMarkdown::from_chunk_range(50..1024))
+        // Generate questions and answers and them to the metadata of the node
+        .then(MetadataQAText::new(openai.clone()))
         .run()
         .await
 }
