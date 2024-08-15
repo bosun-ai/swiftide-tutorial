3799a7324268a873d66dcbd1a0e427de1c95a437
Timon Vonk <timonv@gmail.com>
2024-08-15 17:47:10 +0200

    Add initial notebook and cli implementation

--- ragas/src/main.rs
+++ ragas/src/main.rs
@@ -6,7 +6,7 @@ use swiftide::{
     query::{
         self,
         answers::Simple,
-        evaluators,
+        evaluators::{self, ragas::EvaluationDataSet},
         query_transformers::{self, GenerateSubquestions},
         states, Query,
     },
@@ -15,13 +15,14 @@ use swiftide::{
 use std::{path::PathBuf, str::FromStr};
 
 use anyhow::{Context as _, Result};
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use indoc::formatdoc;
-use qdrant_client::qdrant::SearchPointsBuilder;
 use swiftide::{
     indexing::Pipeline,
     integrations::{openai::OpenAI, qdrant::Qdrant, redis::Redis, treesitter::SupportedLanguages},
 };
+
+const COLLECTION_NAME: &str = "swiftide-ragas";
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -32,7 +33,26 @@ struct Args {
     #[arg(short, long, default_value = "./")]
     path: PathBuf,
 
-    queries: Vec<String>,
+    #[command(flatten)]
+    dataset: DatasetArg,
+
+    #[arg(short, long, default_value = "false")]
+    record_ground_truth: bool,
+
+    #[arg(short, long)]
+    output: PathBuf,
+}
+
+#[derive(clap::Args, Debug, Clone)]
+#[group(required = true, multiple = false)]
+struct DatasetArg {
+    file: Option<PathBuf>,
+    questions: Option<Vec<String>>,
+}
+
+struct Context {
+    openai: OpenAI,
+    qdrant: Qdrant,
 }
 
 #[tokio::main]
@@ -43,7 +63,7 @@ async fn main() -> Result<()> {
 
     let openai = OpenAI::builder()
         .default_embed_model("text-embedding-3-small")
-        .default_prompt_model("gpt-3.5-turbo")
+        .default_prompt_model("gpt-4o-mini")
         .build()?;
 
     let qdrant = Qdrant::builder()
@@ -51,27 +71,29 @@ async fn main() -> Result<()> {
         .collection_name("swiftide-ragas")
         .build()?;
 
-    index_all(&args.language, &args.path, &openai, &qdrant).await?;
+    let context = Context { openai, qdrant };
 
-    let openai = OpenAI::builder()
-        .default_embed_model("text-embedding-3-small")
-        .default_prompt_model("gpt-4o")
-        .build()?;
+    force_delete_qdrant_collection(&context).await?;
 
-    let response = query(&openai, &qdrant, &args.queries).await?;
-    println!(
-        "{}",
-        response
-            .into_iter()
-            .map(|q| q.answer().to_string())
-            .collect::<Vec<_>>()
-            .join("\n")
-    );
+    index_all(&args.language, &args.path, &context).await?;
+
+    let dataset: EvaluationDataSet = if let Some(path) = args.dataset.file {
+        std::fs::read_to_string(path)?.parse()?
+    } else {
+        args.dataset
+            .questions
+            .ok_or(anyhow::anyhow!("Expected questions"))?
+            .into()
+    };
+    let evaluation = query(dataset, args.record_ground_truth, &context).await?;
+
+    let json = evaluation.to_json().await;
+    std::fs::write(args.output, json).context("Failed to write ragas.json")?;
 
     Ok(())
 }
 
-async fn index_all(language: &str, path: &PathBuf, openai: &OpenAI, qdrant: &Qdrant) -> Result<()> {
+async fn index_all(language: &str, path: &PathBuf, context: &Context) -> Result<()> {
     tracing::info!(path=?path, language, "Indexing code");
 
     let language = SupportedLanguages::from_str(language)?;
@@ -83,7 +105,7 @@ async fn index_all(language: &str, path: &PathBuf, openai: &OpenAI, qdrant: &Qdr
             .with_concurrency(50)
             .filter_cached(Redis::try_from_url(
                 "redis://localhost:6379",
-                "swiftide-tutorial",
+                COLLECTION_NAME,
             )?)
             .split_by(|node| {
                 // Any errors at this point we just pass to 'markdown'
@@ -93,45 +115,67 @@ async fn index_all(language: &str, path: &PathBuf, openai: &OpenAI, qdrant: &Qdr
                 node.path.extension().map_or(true, |ext| ext == "md")
             });
 
-    code = code
-        // Uses tree-sitter to extract best effort blocks of code. We still keep the minimum
-        // fairly high and double the chunk size
-        .then_chunk(ChunkCode::try_for_language_and_chunk_size(
-            language,
-            50..1024,
-        )?)
-        .then(MetadataQACode::new(openai.clone()));
+    if cfg!(feature = "chunk") {
+        code = code
+            // Uses tree-sitter to extract best effort blocks of code. We still keep the minimum
+            // fairly high and double the chunk size
+            .then_chunk(ChunkCode::try_for_language_and_chunk_size(
+                language,
+                50..1024,
+            )?);
+    }
 
-    markdown = markdown
-        .then_chunk(ChunkMarkdown::from_chunk_range(50..1024))
+    if cfg!(feature = "metadata") {
+        code = code.then(MetadataQACode::new(context.openai.clone()));
+    }
+
+    if cfg!(feature = "chunk") {
+        markdown = markdown.then_chunk(ChunkMarkdown::from_chunk_range(50..1024));
         // Generate questions and answers and them to the metadata of the node
-        .then(MetadataQAText::new(openai.clone()));
+    }
+
+    if cfg!(feature = "metadata") {
+        markdown = markdown.then(MetadataQAText::new(context.openai.clone()));
+    }
 
     code.merge(markdown)
-        .then_in_batch(50, Embed::new(openai.clone()))
-        .then_store_with(qdrant.clone())
+        .then_in_batch(50, Embed::new(context.openai.clone()))
+        .then_store_with(context.qdrant.clone())
         .run()
         .await
 }
 
 async fn query(
-    openai: &OpenAI,
-    qdrant: &Qdrant,
-    questions: &[String],
-) -> Result<Vec<Query<states::Answered>>> {
+    questions: EvaluationDataSet,
+    record_ground_truth: bool,
+    context: &Context,
+) -> Result<evaluators::ragas::Ragas> {
     let ragas = evaluators::ragas::Ragas::from_prepared_questions(questions);
 
     let pipeline = query::Pipeline::default()
         .evaluate_with(ragas.clone())
-        .then_transform_query(GenerateSubquestions::from_client(openai.clone()))
-        .then_transform_query(query_transformers::Embed::from_client(openai.clone()))
-        .then_retrieve(qdrant.clone())
-        .then_answer(Simple::from_client(openai.clone()));
+        .then_transform_query(GenerateSubquestions::from_client(context.openai.clone()))
+        .then_transform_query(query_transformers::Embed::from_client(
+            context.openai.clone(),
+        ))
+        .then_retrieve(context.qdrant.clone())
+        .then_answer(Simple::from_client(context.openai.clone()));
 
-    let answers = pipeline.query_all(ragas.questions().await).await;
+    pipeline.query_all(ragas.questions().await).await?;
 
-    let json = ragas.to_json().await;
-    std::fs::write("ragas.json", json).context("Failed to write ragas.json")?;
+    if record_ground_truth {
+        ragas.record_answers_as_ground_truth().await;
+    }
 
-    answers
+    Ok(ragas)
+}
+
+async fn force_delete_qdrant_collection(context: &Context) -> Result<()> {
+    context
+        .qdrant
+        .client()
+        .delete_collection(COLLECTION_NAME)
+        .await;
+
+    Ok(())
 }
