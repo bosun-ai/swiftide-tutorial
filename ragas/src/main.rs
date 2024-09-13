a76e259f9e7b3ba625211702d3562d3d129fa65b
Timon Vonk <timonv@gmail.com>
2024-09-13 11:32:25 +0200

    Add more comments

--- ragas/src/main.rs
+++ ragas/src/main.rs
@@ -66,11 +66,13 @@ async fn main() -> Result<()> {
 
     let args = Args::parse();
 
+    // Initialize the OpenAI client
     let openai = OpenAI::builder()
         .default_embed_model("text-embedding-3-small")
         .default_prompt_model("gpt-4o-mini")
         .build()?;
 
+    // Initialize the Qdrant client
     let qdrant = Qdrant::builder()
         .vector_size(1536)
         .collection_name(COLLECTION_NAME)
@@ -78,10 +80,14 @@ async fn main() -> Result<()> {
 
     let context = Context { openai, qdrant };
 
+    // Delete the collection if it already exists
     force_delete_qdrant_collection(&context).await?;
 
+    // Index the code
     index_all(&args.language, &args.path, &context).await?;
 
+    // Either load the dataset from a file or use the questions provided
+    // Then create the evaluation dataset to be used
     let dataset: EvaluationDataSet = if let Some(path) = args.dataset.file {
         std::fs::read_to_string(path)?.parse()?
     } else {
@@ -90,8 +96,11 @@ async fn main() -> Result<()> {
             .ok_or(anyhow::anyhow!("Expected questions"))?
             .into()
     };
+
+    // Query the indexed dataset and return the evaluation
     let evaluation = query(dataset, args.record_ground_truth, &context).await?;
 
+    // Write the evaluation to a json file so it can be used in the python notebook
     let json = evaluation.to_json().await;
     std::fs::write(args.output, json).context("Failed to write ragas.json")?;
 
@@ -105,16 +114,19 @@ async fn index_all(language: &str, path: &PathBuf, context: &Context) -> Result<
     let mut extensions = language.file_extensions().to_owned();
     extensions.push("md");
 
-    let (mut markdown, mut code) =
-        Pipeline::from_loader(FileLoader::new(path).with_extensions(&extensions))
-            .with_concurrency(50)
-            .split_by(|node| {
-                // Any errors at this point we just pass to 'markdown'
-                let Ok(node) = node else { return true };
+    // Index all code and markdown files in the provided directory
+    let (mut markdown, mut code) = Pipeline::from_loader(
+        FileLoader::new(path).with_extensions(&extensions),
+    )
+    .split_by(|node| {
+        // Any errors at this point we just pass to 'markdown'
+        let Ok(node) = node else { return true };
 
-                // On true we go 'markdown', on false we go 'code'.
-                node.path.extension().map_or(true, |ext| ext == "md")
-            });
+        // On true we go 'markdown', on false we go 'code'.
+        node.path.extension().map_or(true, |ext| ext == "md")
+    });
+
+    // For each feature that we want to test, enable them conditionally
 
     if cfg!(feature = "chunk") {
         code = code
@@ -124,21 +136,16 @@ async fn index_all(language: &str, path: &PathBuf, context: &Context) -> Result<
                 language,
                 50..1024,
             )?);
+
+        markdown = markdown.then_chunk(ChunkMarkdown::from_chunk_range(50..1024));
     }
 
     if cfg!(feature = "metadata") {
         code = code.then(MetadataQACode::new(context.openai.clone()));
-    }
-
-    if cfg!(feature = "chunk") {
-        markdown = markdown.then_chunk(ChunkMarkdown::from_chunk_range(50..1024));
-        // Generate questions and answers and them to the metadata of the node
-    }
-
-    if cfg!(feature = "metadata") {
         markdown = markdown.then(MetadataQAText::new(context.openai.clone()));
     }
 
+    // Merge both pipelines and generate embeddings
     code.merge(markdown)
         .then_in_batch(50, Embed::new(context.openai.clone()))
         .then_store_with(context.qdrant.clone())
@@ -151,8 +158,11 @@ async fn query(
     record_ground_truth: bool,
     context: &Context,
 ) -> Result<evaluators::ragas::Ragas> {
+    // Create a new evaluator with prepared questions, either from the input file or the provided
+    // questions
     let ragas = evaluators::ragas::Ragas::from_prepared_questions(questions);
 
+    // Run a query pipeline that answers all provided questions
     let pipeline = query::Pipeline::default()
         .evaluate_with(ragas.clone())
         .then_transform_query(GenerateSubquestions::from_client(context.openai.clone()))
@@ -164,6 +174,10 @@ async fn query(
 
     pipeline.query_all(ragas.questions().await).await?;
 
+    // If the flag is set, record the answers as ground truth.
+    // Ragas needs to know the correct answers to evaluate certain metrics.
+    //
+    // Can also be set manually or have RAGAS handle it. There are pros and cons to each.
     if record_ground_truth {
         ragas.record_answers_as_ground_truth().await;
     }
